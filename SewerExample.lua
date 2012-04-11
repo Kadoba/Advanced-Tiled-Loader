bc101ab8411532125fe27ebeef62e89f464ba7c0
Kadoba <Casey_Baxter@Hotmail.com>
2012-04-11 11:32:20 -0500

    Update SewerExample.lua

--- SewerExample.lua
+++ SewerExample.lua
@@ -1,6 +1,5 @@
-
 -- Setup
-local loader = require("AdvTiledLoader/loader")
+local loader = require("AdvTiledLoader/Loader")
 loader.path = "maps/"
 local map = loader.load("sewers.tmx")
 local SewerExample = {}
