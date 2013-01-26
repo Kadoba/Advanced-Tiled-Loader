e94980288560fdbd49cdf7f36453fe3818fbdc1a
Xenodora <xenodora@gmail.com>
2013-01-26 19:42:26 +0930

    Added a detailed error message for when it can't make a directory while saving a map. Ellipse objects added to the saving process. bugfix: Call to _compactProperties from _compactObject() had somehow lost its '_'.

--- Loader.lua
+++ Loader.lua
@@ -100,6 +100,9 @@ function Loader.save(map, filename)
     local mapx = Loader._compactMap(map)
     if not love.filesystem.exists(Loader.saveDirectory) then
         love.filesystem.mkdir(Loader.saveDirectory)
+	if not love.filesystem.exists(Loader.saveDirectory) then
+	    error("Could not create map save directory '" .. Loader.saveDirectory .. "'. \n-  Write access to '" .. love.filesystem.getSaveDirectory() .. "' denied or read only?\n-  Identity not set in 'conf.lua' or by love.filesystem.setIdentity()?")
+	end
     end
     love.filesystem.write( Loader.saveDirectory .. "/" .. filename, xml.table_to_string(mapx) )
 end
@@ -720,9 +723,14 @@ function Loader._compactObject(object)
         objectx[#objectx+1] = polygonx
     end
 
+    -- <ellipse>
+    if object.ellipse then
+	objectx[#objectx+1] = {label = "ellipse", xarg = {}}
+    end
+
     -- <properties>
     if next(object.properties) then
-        objectx[#objectx+1] = Loader.compactProperties(object.properties)
+        objectx[#objectx+1] = Loader._compactProperties(object.properties)
     end
     
     return objectx
